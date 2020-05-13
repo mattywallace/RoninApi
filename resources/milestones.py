996ce542e21155de597f1d9755c7944c27f7afcd
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-05-12 23:56:42 -0500

    the index route for the milestones, needs to be tweeked slightly

--- resources/milestones.py
+++ resources/milestones.py
@@ -6,9 +6,23 @@ from flask_login import current_user, login_required
 
 milestones = Blueprint('milestones', 'milestones')
 
-@milestones.route('/', methods=['GET'])
-def milestones_test():
-	return "milestones resource working"
+
+# this route needs to be changed so that it will only see the 
+# milestones form the course_id
+
+@milestones.route('/<course_id>', methods=['GET'])
+@login_required
+def milestones_index(course_id):
+	result = models.Milestone.select()
+	print('here is the result' , result)
+	course = models.Course.get_by_id(course_id)
+	milestone_dicts = [model_to_dict(milestone) for milestone in result]
+	return jsonify({
+		"data": milestone_dicts,
+		"message": f"There are currently {len(milestone_dicts)} milestones in the {course.course_name} course.",
+		"status": 200
+	}), 200
+
 
 @milestones.route('/<course_id>', methods=['POST'])
 @login_required
