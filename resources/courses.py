dc3d8cae1f24e7e4d6c5c2ab93620e585763f590
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-05-12 20:50:05 -0500

    user can delete and update the course

--- resources/courses.py
+++ resources/courses.py
@@ -1,7 +1,7 @@
 import models 
 from flask import Blueprint, request, jsonify
 from playhouse.shortcuts import model_to_dict
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 
 
@@ -22,6 +22,7 @@ def courses_index():
 	
 	
 @courses.route('/list', methods=['GET'])
+@login_required
 def user_course_index():
 	current_user_course_dicts = [model_to_dict(course) for course in current_user.courses]
 	for course_dict in current_user_course_dicts:
@@ -34,6 +35,7 @@ def user_course_index():
 			}), 200
 
 @courses.route('/create', methods=['POST'])
+@login_required
 def create_course():
 	payload = request.get_json()
 	payload['course_name'] = payload['course_name'].lower()
@@ -55,14 +57,41 @@ def create_course():
 			)
 	new_course_dict = model_to_dict(new_course)
 	new_course_dict['administrator'].pop('password')
-	print(course)
 	return jsonify(
 		data=course_dict,
 		message=f"User {new_course_dict['adminstrator']} has successfully created a new class called {new_course_dict['course_name']}",
 		status=201
 	),201 
 
+@courses.route('/<id>', methods=['PUT'])
+@login_required
+def update_dog(id):
+	payload = request.get_json()
+	update_query = models.Course.update(
+		course_name=payload['course_name'],
+		course_keywords=payload['course_keywords'],
+		description=payload['description'],
+		certification=payload['certification']
+	).where(models.Course.id == id)
+	num_of_rows_modified = update_query.execute()
+	updated_course = models.Course.get_by_id(id)
+	updated_course_dict = model_to_dict(updated_course)
+	return jsonify(
+		data=updated_course_dict,
+		message=f"Successfully updated course with id {id}",
+		status=200
+	), 200
 
+@courses.route('/<id>', methods=['DELETE'])
+def delete_course(id):
+	delete_query = models.Course.delete().where(models.Course.id == id)
+	num_of_rows_deleted = delete_query.execute()
+	print(num_of_rows_deleted)
+	return jsonify(
+		data={},
+		message="Successfully deleted {} course with the id {}".format(num_of_rows_deleted, id),
+		status=200,
+	), 200
 	
 
 
