d9f4fc11206dd3cc9d7b0cabdc75dcf4777d0b0e
matthew wallace <matthewwallace@matthews-MacBook-Pro-2.local>
2020-05-13 04:09:45 -0500

    update submission route sending back 200 response but not changin anything

--- resources/submissions.py
+++ resources/submissions.py
@@ -26,5 +26,35 @@ def create_submission(user_id, milestone_id):
 		status=201
 	),201 
 
+@submissions.route('/<milestone_id>/<id>', methods=['GET'])
+@login_required
+def show_submission(milestone_id, id):
+	submission = models.Submission.get_by_id(id)
+	submission_dict = model_to_dict(submission)
+	print('here is the submission dict', submission_dict)
+	submission_dict['student_subbed'].pop('password')
+	submission_dict['milestone_sub']['course_from']['administrator'].pop('password')
+	return jsonify(
+		data=submission_dict,
+		message=f"Here is the submission from user {submission_dict['student_subbed']['username']} for milestone {milestone_id} in course {submission_dict['milestone_sub']['course_from']['course_name']} ",
+		status=200
+	), 200
 
-
+@submissions.route('/<milestone_id>/<id>', methods=['PUT'])
+@login_required
+def submission_update(milestone_id, id):
+	payload = request.get_json()
+	update_query = models.Submission.update(	
+		approved=payload['approved']
+	).where(models.Submission.id == id)
+	num_of_rows_modified = update_query.execute()
+	submission = models.Submission.get_by_id(id)
+	submission_dict = model_to_dict(submission)
+	print('here is the updated submission dict', submission_dict)
+	submission_dict['student_subbed'].pop('password')
+	submission_dict['milestone_sub']['course_from']['administrator'].pop('password')
+	return jsonify(
+		data=submission_dict,
+		message=f"Successfully updated submission {id} from course {submission_dict['milestone_sub']['course_from']['course_name']} submitted by {submission_dict['student_subbed']['username']}",
+		status=200
+	), 200
